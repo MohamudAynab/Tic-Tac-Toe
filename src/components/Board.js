12195c9c9173a80a7a8cbe720847c4ad50d0ae76
MohamudAynab <mfaynab88@gmail.com>
2023-08-16 23:00:12 +0000

    update to components folder.

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "/workspaces/Tic-Tac-Toe/src/style.css";
+import Square from "./Square";
+
+
+function Board({ rows }) {
+    <><div>
+        <Square />
+    </div><div>{rows}</div></>;
+  }
+
+  export default Board;
